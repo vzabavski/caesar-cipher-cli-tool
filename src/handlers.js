c89988a9f6337ec704f3c31dfcfa7e33e64fcd09
vzabavski <vzabavski123@gmail.com>
2021-05-08 15:24:25 +0300

    refactor: change structure of code in handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -3,35 +3,33 @@ const caesarCipher = require('./cipher');
 const { Writable, Transform } = require('stream');
 
 const inputHandler = (input) => {
-  if(input) {
-    return fs.createReadStream(input, 'utf-8');
-  } else {
+  if(!input) {
     return process.stdin.on('data', (chunk) => {
       return chunk
-    })
+    });
   }
+  return fs.createReadStream(input, 'utf-8');
 };
 
 const transformHandler = (action, shift) => {
-    const stream = new Transform();
-    stream._transform = (chunk, encoding, callback) => {
-        const modified = caesarCipher(action, +shift, chunk.toString());
-        callback(null, modified);
-    }
-    return stream;
+  const stream = new Transform();
+  stream._transform = (chunk, encoding, callback) => {
+    const modified = caesarCipher(action, +shift, chunk.toString());
+    callback(null, modified);
+  }
+  return stream;
 }
 
 const outputHandler = (output) => {
-  if(output) {
-    return fs.createWriteStream(output, {flags: 'a'}, 'utf-8')
-  } else {
+  if(!output) {
     const stream = new Writable();
     stream._write = function(chunk, encoding, next) {
       process.stdout.write(chunk);
-      next()
+      next();
     }
     return stream;
   }
+  return fs.createWriteStream(output, {flags: 'a'}, 'utf-8')
 }
 
 module.exports = { inputHandler, transformHandler, outputHandler }
